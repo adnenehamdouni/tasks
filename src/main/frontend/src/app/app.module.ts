cfa9e633ddfecdb94eb07348b8ac1525d0b7ded8
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-01-19 11:25:09 +0100

    Add new components : Tasks-Add and Tasks-List

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TaskComponent } from './components/tasks/task/task.component';
+import { TasksAddComponent } from './components/tasks/tasks-add/tasks-add.component';
+import { TasksListComponent } from './components/tasks/tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TasksAddComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule
