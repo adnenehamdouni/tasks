7515094df77b97f8f07a4082abd775b66d33d5df
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-01-26 12:45:20 +0100

    init list from taksList created on component

--- src/main/frontend/src/app/components/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/components/tasks/tasks-list/tasks-list.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import {Task} from "../../../shared/model/task.model";
+import { Component, OnInit } from "@angular/core";
+import { Task } from "../../../shared/model/task.model";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.scss']
+  selector: "app-tasks-list",
+  templateUrl: "./tasks-list.component.html",
+  styleUrls: ["./tasks-list.component.scss"]
 })
 export class TasksListComponent implements OnInit {
-
   tasks: Task[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.initTasks();
   }
 
   private initTasks() {
-    console.log('TasksListComponent: initTasks here');
+    console.log("TasksListComponent: initTasks here");
     this.tasks.push(new Task(1, "Task 1", true, "26/01/2019"));
     this.tasks.push(new Task(2, "Task 2", false, "26/01/2019"));
     this.tasks.push(new Task(3, "Task 3", false, "26/01/2019"));
+  }
+
+  onTaskChange(event: any, task: Task) {
+    //this.task.se
+  }
+
+  getDueDateLabel(task: Task) {
+    return task.completed ? "label-success" : "label-primary";
   }
 }
